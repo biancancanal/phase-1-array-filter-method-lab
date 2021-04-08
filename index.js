ef48b10433ab1589c59784d33ecbfe7cce62ffa9
Bianca <biancancanal@gmail.com>
2021-04-08 17:31:28 -0500

    commit tree 58ddc254cd2ea7cd538bd896c8fe68f030a25b9d

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solution here
+
+function findMatching(drivers, name) {
+    return (drivers.filter((driver) => name.toUpperCase() == driver.toUpperCase()))
+}
+
+function fuzzyMatch(drivers, letter) {
+    let firstLetter = drivers[0]
+    return (drivers.filter((firstLetter) => letter == firstLetter))
+}
